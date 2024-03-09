733dbd452262af6ca8c6a4642875ace05d304f0e
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-03-09 22:57:20 +0100

    function displayMain()

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 // Bouton Validate Start
 
+// function changeText() {
+//   let element = document.getElementById("btn-start");
+
+//   // @ts-ignore
+//   if (element.value == "Validate") {
+//     // @ts-ignore
+//     element.value = "Start";
+//     // @ts-ignore
+//   } else element.value = "Validate";
+// }
+
+// let questionDisplay = document.getElementById("main");
+
+function displayMain() {
+  let element = document.getElementById("btn-start");
+  let questionDisplay = document.getElementById("main");
+  let headerDisplay = document.getElementById("main-page");
+
+  // @ts-ignore
+  if (element.value === "Validate") {
+    // @ts-ignore
+    questionDisplay.style.display = "block";
+    // @ts-ignore
+    headerDisplay.style.display = "none";
+
+    // @ts-ignore
+    element.value = "Start";
+  } else {
+    // headerDisplay.style.display = "block";
+    // @ts-ignore
+    questionDisplay.style.display = "block";
+    // @ts-ignore
+    element.value = "Validate";
+  }
+}
+// questionDisplay.style.display = "block";
 function changeText() {
   let element = document.getElementById("btn-start");
+
   // @ts-ignore
   if (element.value == "Validate") {
     // @ts-ignore
@@ -9,3 +46,13 @@ function changeText() {
     // @ts-ignore
   } else element.value = "Validate";
 }
+
+// function masquer_div(id)
+// {
+//   if (document.getElementById(id).style.display == 'none') {
+//        document.getElementById(id).style.display = 'block';
+//   }
+//   else {
+//        document.getElementById(id).style.display = 'none';
+//   }
+// }
