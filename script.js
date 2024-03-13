8acee30d67d78e3859d71193fd3973b0fdbedf7b
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-03-13 14:39:10 +0100

    creation script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const displayHeader = document.getElementById("header");
+const displayMain = document.getElementById("main");
+const displayBoutonFooter = document.getElementById("btn-div-end");
+const startBouton = document.getElementById("sButton");
+
+startBouton.addEventListener("click", () => {
+  displayHeader.style.display = "none";
+});
