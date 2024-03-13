56e9ac1618a6d45cbd60e833cc1959ce331aabf4
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-03-13 14:53:02 +0100

    display main header front page

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const displayHeader = document.getElementById("header");
 const displayMain = document.getElementById("main");
 const displayBoutonFooter = document.getElementById("btn-div-end");
 const startBouton = document.getElementById("sButton");
+const nextBouton = document.getElementById("nButton");
+
+// function displayStartButton() {
+//   startBouton.style.display = "none";
+// }
+// displayStartButton();
+// startBouton.style.display = "none";
+displayMain.style.display = "none";
+nextBouton.style.display = "none";
 
 startBouton.addEventListener("click", () => {
   displayHeader.style.display = "none";
+  displayMain.style.display = "block";
+  nextBouton.style.display = "block";
 });
